2a176eb61a16b1665e88fdc5b95d36149ed43aa9
sirishatelugu <140062169+sirishatelugu@users.noreply.github.com>
2023-12-31 09:02:11 +0530

    Update StaffController.java

--- StaffController.java
+++ StaffController.java
@@ -1,6 +1,6 @@
 package com.lifecare.Lifecare.controller;
 
-import com.lifecare.Lifecare.repository.StaffRepository; // Replace PatientRepository with StaffRepository
+import com.lifecare.Lifecare.repository.StaffRepository; 
 import org.apache.catalina.connector.Response;
 import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,46 +8,46 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import com.lifecare.Lifecare.entity.Staff; // Replace Patient with Staff
-import com.lifecare.Lifecare.service.StaffService; // Replace PatientService with StaffService
+import com.lifecare.Lifecare.entity.Staff; 
+import com.lifecare.Lifecare.service.StaffService; 
 
 @Controller
-public class StaffController { // Replace PatientController with StaffController
-    private StaffService staffService; // Replace PatientService with StaffService
+public class StaffController { 
+    private StaffService staffService; 
 
     @Autowired
-    public StaffController(StaffService staffService) { // Replace PatientController with StaffController, PatientService with StaffService
+    public StaffController(StaffService staffService) {  
         super();
-        this.staffService = staffService; // Replace patientService with staffService
+        this.staffService = staffService;
     }
 
-    @GetMapping("/staff") // Replace patients with staff
-    public String listStaff(Model model) { // Replace listPatients with listStaff
-        model.addAttribute("staff", staffService.getAllStaff()); // Replace patients with staff
-        return "staff"; // Replace patients with staff
+    @GetMapping("/staff") 
+    public String listStaff(Model model) { 
+        model.addAttribute("staff", staffService.getAllStaff());
+        return "staff"; 
     }
 
-    @GetMapping("/staff/new") // Replace patients with staff
-    public String createStaff(Model model) { // Replace createPatient with createStaff
-        // create staff object to hold staff form data
-        Staff staff = new Staff(); // Replace Patient with Staff
-        model.addAttribute("staff", staff); // Replace patients with staff
-        return "createstaff"; // Replace createpatient with createstaff
+    @GetMapping("/staff/new") 
+    public String createStaff(Model model) { 
+      
+        Staff staff = new Staff(); 
+        model.addAttribute("staff", staff);
+        return "createstaff"; 
     }
 
-    @PostMapping("/staff") // Replace patients with staff
-    public String saveStaff(@ModelAttribute("staff") Staff staff) { // Replace savePatient with saveStaff, Patient with Staff
-        staffService.saveStaff(staff); // Replace patientService with staffService
-        return "redirect:/staff"; // Replace patients with staff
+    @PostMapping("/staff") 
+    public String saveStaff(@ModelAttribute("staff") Staff staff) { 
+        staffService.saveStaff(staff); 
+        return "redirect:/staff"; 
     }
 
-    @GetMapping("/staff/edit/{id}") // Replace patients with staff
-    public String editStaffForm(@PathVariable Long id, Model model) { // Replace editPatientForm with editStaffForm, Patient with Staff
-        model.addAttribute("staff", staffService.getStaffById(id)); // Replace patientService with staffService, Patient with Staff
-        return "editstaff"; // Replace editpatient with editstaff
+    @GetMapping("/staff/edit/{id}") 
+    public String editStaffForm(@PathVariable Long id, Model model) { 
+        model.addAttribute("staff", staffService.getStaffById(id));
+        return "editstaff"; 
     }
 
-    @PostMapping("/staff/{id}") // Replace patients with staff
+    @PostMapping("/staff/{id}") 
     public String updateStaff(@PathVariable Long id,
                                 @ModelAttribute("staff") Staff staff,
                                 Model model) {
@@ -63,17 +63,16 @@ public class StaffController { // Replace PatientController with StaffController
 
         // save updated staff object
         staffService.updateStaff(existingStaff);
-        return "redirect:/staff"; // Replace patients with staff
+        return "redirect:/staff"; 
     }
 
-    @GetMapping("/staff/{id}") // Replace patients with staff
-    public String deleteStaff(@PathVariable Long id) { // Replace deletePatient with deleteStaff
-        staffService.deleteStaffById(id); // Replace patientService with staffService
-        return "redirect:/staff"; // Replace patients with staff
-    }
+    @GetMapping("/staff/{id}") 
+    public String deleteStaff(@PathVariable Long id) { 
+        staffService.deleteStaffById(id);
+        return "redirect:/staff"; 
 
     @Autowired
-    private StaffRepository staffRepository; // Replace PatientRepository with StaffRepository
+    private StaffRepository staffRepository; 
 
     @GetMapping("/searchpage")
     public String searchPage() {
@@ -82,7 +81,7 @@ public class StaffController { // Replace PatientController with StaffController
 
     @GetMapping("/staffdetails")
     public String detailsPage(@RequestParam Long id, Model model) {
-        Staff entity = staffRepository.findById(id).orElse(null); // Replace Patient with Staff
+        Staff entity = staffRepository.findById(id).orElse(null);
         model.addAttribute("entity", entity);
         return "staffdetails";
     }
